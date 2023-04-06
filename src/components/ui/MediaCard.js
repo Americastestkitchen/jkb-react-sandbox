eb179727c0f5c9ffa3a1de5af9f0d1163f704592
Joshua Briley <joshua.briley@americastestkitchen.com>
2023-04-06 14:27:42 -0400

    A11Y updates.

--- src/components/ui/MediaCard.js
+++ src/components/ui/MediaCard.js
@@ -42,12 +42,12 @@ const MediaCard = ({elementType = 'div', headerLevel = 'h2', id, ...props}) => {
             {document_title}
           </Header>
           <div className="relative z-10 w-4 h-4 shrink-0">
-            <button onClick={toggleMenu} className="font-bold">...</button>
+            <button aria-label={`Update ${document_title}`} onClick={toggleMenu} className="font-bold">...</button>
           </div>
         </div>
         <div className="flex items-center gap-6">
           {document_avg_score && (
-            <aside aria-label="Item reviews" className="flex items-center pt-2">
+            <aside aria-label={`${document_title} item reviews`} className="flex items-center pt-2">
               <svg className="block w-5 h-5 text-mint-dark" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 15.2 14.5">
                 <path fill="currentColor" d="M7.6 11l-4.7 3.5 1.8-5.6L0 5.5h5.8L7.6 0l1.8 5.5h5.8l-4.7 3.4 1.8 5.6L7.6 11z"></path>
               </svg>
@@ -60,7 +60,7 @@ const MediaCard = ({elementType = 'div', headerLevel = 'h2', id, ...props}) => {
             </aside>
           )}
           {document_comment_count > 0 && (
-            <aside aria-label="Comments" className='flex items-center gap-1 pt-2'>
+            <aside aria-label={`${document_title} comments`}  className='flex items-center gap-1 pt-2'>
               <svg className="w-4 h-4 text-mint-dark" aria-hidden="true" focusable="false" height="13.284" viewBox="0 0 13.279 13.284" xmlns="http://www.w3.org/2000/svg" width="13.279"><g transform="translate(-13.993 -7.02)"><path d="M27.26,13.252A6.64,6.64,0,1,0,20.452,20.3a6.394,6.394,0,0,0,3.081-.664,1.69,1.69,0,0,1,1.125-.092l2.14.609a.27.27,0,0,0,.332-.332l-.646-1.993a1.594,1.594,0,0,1,.092-1.2A6.653,6.653,0,0,0,27.26,13.252Z" fill="currentColor"></path></g></svg>
               <p>
                 <span className="font-bold">{document_comment_count}</span>
