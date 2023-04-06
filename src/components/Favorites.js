b3771fe359db4a7de2c6d3fbb0cf7c38e5d7898e
Joshua Briley <joshua.briley@americastestkitchen.com>
2023-04-06 13:42:43 -0400

    User controlled enpoints.

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -7,6 +7,7 @@ const Favorites = () => {
   const [pagination, setPagination] = useState(null)
   const [currentSort, setCurrentSort] = useState()
   const [isOpen, setIsOpen] = useState(false)
+  const [endpoint, setEndpoint] = useState('recipes')
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -15,7 +16,7 @@ const Favorites = () => {
   useEffect(() => {
     const fetchData = async () => {
     try {
-      const response = await fetch('http://localhost:3001/favorites')
+      const response = await fetch(`http://localhost:3001/favorite_${endpoint}`)
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
@@ -28,7 +29,11 @@ const Favorites = () => {
   }
 
   fetchData()
-  }, [])
+  }, [endpoint])
+
+  const handleEndpointClick = (newEndpoint) => {
+    setEndpoint(newEndpoint);
+  };
 
   const sortByPublishDate = () => {
     const sortedResults = [...favorites].sort((a, b) => {
@@ -92,20 +97,35 @@ const Favorites = () => {
         <p className="font-bold">Sort and Filter By</p>
         <ul className="space-y-1">
           <li>
-            <button aria-label="Sort by publish date" className={currentSort === 'date' ? 'menu-button active' : 'menu-button'} onClick={sortByPublishDate}>
-              <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-              </svg>
+            <button aria-label="Sort by publish date" className='menu-button' onClick={sortByPublishDate}>
+              <span className={`h-4 w-4 rounded-full border transition-colors duration-150 ${currentSort === 'date' ? 'bg-mint border-mint' : 'border-silver group-hover:bg-mint'}`}></span>
               Sort by publish date
             </button>
           </li>
           <li>
-            <button aria-label="Sort alphabetically" className={currentSort === 'alphabetical' ? 'menu-button active' : 'menu-button'} onClick={sortByAlphabetical}>
-              <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
-              </svg>
+            <button aria-label="Sort alphabetically" className='menu-button' onClick={sortByAlphabetical}>
+              <span className={`h-4 w-4 rounded-full border transition-colors duration-150 ${currentSort === 'alphabetical' ? 'bg-mint border-mint' : 'border-silver group-hover:bg-mint'}`}></span>
               Sort alphabetically
             </button>
+          </li>
+        </ul>
+
+        <ul className="space-y-1">
+          <li>
+            <button className='menu-button' onClick={() => handleEndpointClick('recipes')}>
+              <span className={`h-4 w-4 rounded-full border transition-colors duration-150 ${endpoint === 'recipes' ? 'bg-mint border-mint' : 'border-silver group-hover:bg-mint'}`}></span>
+              Recipes
+            </button>
+          </li>
+          <li>
+            <button className='menu-button' onClick={() => handleEndpointClick('taste_tests')}>
+              <span className={`h-4 w-4 rounded-full border transition-colors duration-150 ${endpoint === 'taste_tests' ? 'bg-mint border-mint' : 'border-silver group-hover:bg-mint'}`}></span>
+              Taste Tests</button>
+          </li>
+          <li>
+            <button className='menu-button' onClick={() => handleEndpointClick('episodes')}>
+              <span className={`h-4 w-4 rounded-full border transition-colors duration-150 ${endpoint === 'episodes' ? 'bg-mint border-mint' : 'border-silver group-hover:bg-mint'}`}></span>
+              Episodes</button>
           </li>
         </ul>
       </SlideInContainer>
